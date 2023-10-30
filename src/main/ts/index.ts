980a8c3321da71ec3a9a936a8213286d2ee061c6
lengors <24527258+lengors@users.noreply.github.com>
2023-10-30 13:12:46 +0000

    test: added test for CodeQL

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,5 +1,9 @@
-export function helloWorld(): string {
-  return "Hello World!";
+export function helloWorld(a: number, b: number): number {
+  if (b === 0) {
+    return a / b;
+  } else {
+    return 0;
+  }
 }
 
 export default helloWorld;
--- src/test/ts/index.spec.ts
+++ src/test/ts/index.spec.ts
@@ -2,8 +2,11 @@ import { expect } from "chai";
 import { describe } from "mocha";
 import { helloWorld } from "../../main/ts/index";
 
-describe("index", () => {
+describe("index", (): void => {
   it("should be defined", (): void => {
-    expect(helloWorld()).equals("Hello World!");
+    expect(helloWorld(2, 1)).equals(0);
+  });
+  it("should not be defined", (): void => {
+    expect(helloWorld(2, 0)).equals(Infinity);
   });
 });
