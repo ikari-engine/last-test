274410719276921cd3a98f3acbeee4db4cc6e1a7
lengors <24527258+lengors@users.noreply.github.com>
2023-11-01 22:24:12 +0000

    ci: setup semantic-release

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,3 +1,8 @@
+/**
+ * A function that returns the text "Hello, World!"
+ *
+ * @returns A string containing the text "Hello, World!"
+ */
 export function helloWorld(): string {
   return "Hello, World!";
 }
