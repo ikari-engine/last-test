d373a3ce5a3051b3a8b6794ebc74923c4341c4d1
lengors <24527258+lengors@users.noreply.github.com>
2023-11-03 01:23:50 +0000

    docs(config): remove current version from gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          includeCurrentVersion: true,
+          includeCurrentVersion: false,
           editUrl: "https://github.com/ikari-engine/last-test/tree/gh-pages/",
         },
         theme: {
