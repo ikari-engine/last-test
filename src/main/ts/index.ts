817c00672ba2c7f50bfcbd2c18660dde02f76967
lengors <24527258+lengors@users.noreply.github.com>
2023-12-17 20:18:35 +0000

    feat: test

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -4,7 +4,7 @@
  * @returns A string containing the text "Hello, World!"
  */
 export function helloWorld(): string {
-  return "Hello, World!";
+  return "Hello, World! This is an even better version!";
 }
 
 export default helloWorld;
